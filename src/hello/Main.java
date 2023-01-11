f892217e6d5db6140e147ddd449851e9a4627185
Nigel Edwards <nigel.edwards@cwa.ac.uk>
2023-01-11 15:24:58 +0000

    Added File access tests

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -1,5 +1,6 @@
 package hello;
 
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
@@ -19,5 +20,30 @@ public class Main {
        }
 
         System.out.println(num);
+
+        try {
+            PrintWriter writer = new PrintWriter(new FileWriter("test.txt"));
+            writer.println("Hello World");
+            writer.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        try
+        {
+            BufferedReader reader = new BufferedReader(new FileReader("test.txt"));
+            String line = null;
+            // First line should be the Zoo's name
+            String data = reader.readLine();
+            reader.close();
+            System.out.println(data);
+            System.out.println(data);
+        }
+        catch(Exception ex)
+        {
+            System.out.println("couldn`t read the file");
+            ex.printStackTrace();
+        }
     }
 }
