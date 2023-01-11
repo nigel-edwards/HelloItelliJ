eadd4cfc03f2bbf4f3ee42a9774612234132f793
Nigel Edwards <nigel.edwards@cwa.ac.uk>
2023-01-11 15:03:35 +0000

    Added scanner tests

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -1,7 +1,23 @@
 package hello;
 
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
+        // ToDo Add some stuff in main
         System.out.println("Hello world!");
+        Scanner keyboard = new Scanner(System.in);
+        int num = 0;
+        try {
+            System.out.print("Please enter an integer: ");
+            num = keyboard.nextInt();
+        }
+       catch (Exception ex)
+       {
+           System.out.println(ex.getMessage());
+       }
+
+        System.out.println(num);
     }
 }
